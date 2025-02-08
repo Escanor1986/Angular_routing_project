3a6f16278a02b9bf58f87b7afc8ce3894793344b
Escanor1986 <lionel.zovi@gmail.com>
2025-02-08 09:53:18 +0100

    Added base routing with routerLink for home, users, page404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { UsersComponent } from './users/users.component';
+import { Page404Component } from './page404/page404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'users', component: UsersComponent },
+  { path: '**', component: Page404Component }
+];
