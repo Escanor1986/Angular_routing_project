9b27374b3dcf3a51bf1beae2cc5a4433a1932ba8
Escanor1986 <lionel.zovi@gmail.com>
2025-02-08 10:30:04 +0100

    Used Router class & navigateByUrl method to navigate to another page

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
@@ -6,6 +7,19 @@ import { Component } from '@angular/core';
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
 
+  constructor(private readonly router: Router) { 
+    this.router = router;
+  }
+
+  ngOnInit() {
+    console.log('Homepage component initialized...');
+    console.log(this.router);
+  }
+
+  public redirection() {
+    this.router.navigateByUrl('users');
+    console.log('Redirecting to users page...');
+  }
 }
